058e962cbfc9b3305bdd5a7fd52ceb2693feeb6f
Yassineben <benbaaliyassine@gmail.com>
2023-09-23 11:25:00 -0700

    Tasks

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("Hello, world!\n");
-    return 0;
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    return (0);
 }
-
